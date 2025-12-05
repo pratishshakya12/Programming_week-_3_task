d466cc4962722d376224611a6240e22129ed84cf
Pratish Shakya <pratishshakya12@gmail.com>
2025-12-05 17:34:03 +0545

    something

--- Question_14.py
+++ Question_14.py
@@ -1,4 +1,4 @@
-planets={1:(0.38),2:("Venus",0.91), 3:("Mars",0.38),4:("Jupiter",2.53),5:("Saturn",1.07),6:("Uranus",0.89),7:("Neptune",1.14)}
+planets={1:0.38,2:(0.91), 3:(0.38),4:(2.53),5:(1.07),6:(0.89),7:(1.14)}
 
 weight=float(input("Enter your earth weight: "))
 num=int(input("Enter planet number (1-7): "))
